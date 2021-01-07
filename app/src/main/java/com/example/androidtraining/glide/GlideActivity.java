d2e98f157181b544e4a0b91654f58658db5b19c2
viral <viral.shah@coviam.com>
2021-01-07 21:48:11 +0530

    glide added

--- app/src/main/java/com/example/androidtraining/glide/GlideActivity.java
+++ app/src/main/java/com/example/androidtraining/glide/GlideActivity.java
@@ -1,10 +1,12 @@
 package com.example.androidtraining.glide;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.androidtraining.R;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GlideActivity extends AppCompatActivity {
 
@@ -12,5 +14,15 @@ public class GlideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glide);
+        ImageView imageView = findViewById(R.id.iv_image);
+        ImageView gifImage = findViewById(R.id.iv_gif);
+        Glide.with(this)
+                .load("https://images.livemint.com/rf/Image-621x414/LiveMint/Period2/2017/04/06/Photos/Processed/munnar2-krXG--621x414@LiveMint.jpg")
+                .placeholder(R.drawable.placeholder)
+                .into(imageView);
+        Glide.with(this).asGif()
+                .load("https://4.bp.blogspot.com/-uhjF2kC3tFc/U_r3myvwzHI/AAAAAAAACiw/tPQ2XOXFYKY/s1600/Circles-3.gif")
+                .into(gifImage);
+
     }
 }
