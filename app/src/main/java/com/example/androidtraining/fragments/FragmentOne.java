1d4e0ae551a3ca08c8234505f4672e95aa7359f4
viral <viral.shah@coviam.com>
2021-01-08 12:48:47 +0530

    lifecycle logs added in fragment

--- app/src/main/java/com/example/androidtraining/fragments/FragmentOne.java
+++ app/src/main/java/com/example/androidtraining/fragments/FragmentOne.java
@@ -2,9 +2,7 @@ package com.example.androidtraining.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
-
-
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,30 +11,85 @@ import android.widget.Button;
 import com.example.androidtraining.R;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentOne extends Fragment {
 
+    private static final String TAG = "FRAGMENT-LIFECYCLE";
+
     private IActivityCommunicator mIActivityCommunicator;
+
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        if(context instanceof IActivityCommunicator){
+        if (context instanceof IActivityCommunicator) {
             mIActivityCommunicator = (IActivityCommunicator) context;
         }
+        Log.d(TAG, "onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_one, container, false);
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
         Button button = view.findViewById(R.id.bt_open);
         button.setOnClickListener(view1 -> {
-            if(null != mIActivityCommunicator){
+            if (null != mIActivityCommunicator) {
                 mIActivityCommunicator.openFragmentTwo();
             }
         });
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.d(TAG, "onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach");
+        super.onDetach();
     }
 }
--- app/src/main/java/com/example/androidtraining/fragments/FragmentsActivity.java
+++ app/src/main/java/com/example/androidtraining/fragments/FragmentsActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.androidtraining.R;
 
 public class FragmentsActivity extends AppCompatActivity implements IActivityCommunicator {
+
+    private static final String TAG = "ACTIVITY-LIFECYCLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,43 @@ public class FragmentsActivity extends AppCompatActivity implements IActivityCom
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.fl_container, fragmentOne);
         fragmentTransaction.commit();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
     }
 
     @Override
